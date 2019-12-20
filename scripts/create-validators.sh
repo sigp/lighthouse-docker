71fdec43107ab0ff98a3fdee5016f689e6160ea3
Paul Hauner <paul@paulhauner.com>
2019-12-21 00:23:20 +1100

    Ensure account manager is skipped without vals

--- scripts/create-validators.sh
+++ scripts/create-validators.sh
@@ -4,29 +4,31 @@
 # $VALIDATOR_COUNT validators. If the directory does exist, the script does
 # nothing.
 
-if [ ! -d ~/.lighthouse/validators ]; then
-	if [ "$SEND_DEPOSITS" != "" ]; then
-		lighthouse \
-			--debug-level $DEBUG_LEVEL \
-			account \
-			validator \
-			new \
-			--send-deposits \
-			--eth1-endpoint $DEPOSITS_ETH1_NODE \
-			--password $PASSWORD_FILE \
-			--deposit-value $DEPOSIT_VALUE \
-			random \
-			$VALIDATOR_COUNT
+if [ "$START_VALIDATOR" != "" ]; then
+	if [ ! -d ~/.lighthouse/validators ]; then
+		if [ "$SEND_DEPOSITS" != "" ]; then
+			lighthouse \
+				--debug-level $DEBUG_LEVEL \
+				account \
+				validator \
+				new \
+				--send-deposits \
+				--eth1-endpoint $DEPOSITS_ETH1_NODE \
+				--password $PASSWORD_FILE \
+				--deposit-value $DEPOSIT_VALUE \
+				random \
+				$VALIDATOR_COUNT
+		else
+			lighthouse \
+				--debug-level $DEBUG_LEVEL \
+				account \
+				validator \
+				new \
+				--deposit-value $DEPOSIT_VALUE \
+				random \
+				$VALIDATOR_COUNT
+		fi
 	else
-		lighthouse \
-			--debug-level $DEBUG_LEVEL \
-			account \
-			validator \
-			new \
-			--deposit-value $DEPOSIT_VALUE \
-			random \
-			$VALIDATOR_COUNT
+		echo "Validator directory already exists. Not creating validators."
 	fi
-else
-	echo "Validator directory already exists. Not creating validators."
 fi
