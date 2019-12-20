ad4b815f49e7de87ff31ecb355a11626d6b292d5
Paul Hauner <paul@paulhauner.com>
2019-12-20 18:00:37 +1100

    Make validator client optional

--- scripts/start-validator-client.sh
+++ scripts/start-validator-client.sh
@@ -4,13 +4,15 @@
 # files (we assume that each file is a validator directory) before starting the
 # validator client.
 
-while [ $(ls -1 ~/.lighthouse/validators | wc -l) != $VALIDATOR_COUNT ]
-do
-	echo "Waiting for $VALIDATOR_COUNT validators."
-	sleep 1
-done
+if [ "$START_VALIDATOR" != "" ]; then
+	while [ $(ls -1 ~/.lighthouse/validators | wc -l) != $VALIDATOR_COUNT ]
+	do
+		echo "Waiting for $VALIDATOR_COUNT validators."
+		sleep 1
+	done
 
-lighthouse \
-	--debug-level $DEBUG_LEVEL \
-	validator \
-	--server http://beacon_node:5052
+	lighthouse \
+		--debug-level $DEBUG_LEVEL \
+		validator \
+		--server http://beacon_node:5052
+fi
