fb54801d25f8c5532e03c0fafe0719c35aec9092
Paul Hauner <paul@paulhauner.com>
2019-12-21 19:12:05 +1100

    Remove old system

--- lighthouse/scripts/run_bootstrapped_node.sh
+++
@@ -1,6 +0,0 @@
-/root/lighthouse/target/release/beacon_node \
-	--debug-level $DEBUG_LEVEL \
-	testnet \
-	--force \
-	bootstrap \
-	http://172.42.0.250:5052
--- lighthouse/scripts/run_validator_client.sh
+++
@@ -1,8 +0,0 @@
-/root/lighthouse/target/release/validator_client \
-	--server $BEACON_NODE \
-	--debug-level $DEBUG_LEVEL \
-	testnet \
-	-b \
-	insecure \
-	$FIRST_VALIDATOR \
-	$NUM_VALIDATORS
