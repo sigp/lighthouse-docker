2fb029ada76a4d461b769f508dba725641bb8029
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-08-03 03:38:23 +0300

    Dont let lighthouse create keys. Instead assume that user adds keys to lighthouse-data/validators/

--- scripts/start-validator-client.sh
+++ scripts/start-validator-client.sh
@@ -1,40 +1,10 @@
 #! /bin/bash
 
-WALLET_NAME=validators
-WALLET_PASSFILE=~/.lighthouse/secrets/$WALLET_NAME.pass
-
 if [ "$TESTNET" != "" ]; then
 	TESTNET_PARAM="--testnet $TESTNET"
 fi
 
 if [ "$START_VALIDATOR" != "" ]; then
-	if [ ! -d ~/.lighthouse/secrets ]; then
-		cd ~/.lighthouse; mkdir secrets
-	fi
-
-	if [ ! -d ~/.lighthouse/wallets ]; then
-		lighthouse \
-			--debug-level $DEBUG_LEVEL \
-			$TESTNET_PARAM \
-			account \
-			wallet \
-			create \
-			--name $WALLET_NAME \
-			--passphrase-file $WALLET_PASSFILE
-	else
-		echo "Wallet directory already exists. Will not create wallet."
-	fi
-
-	lighthouse \
-		--debug-level $DEBUG_LEVEL \
-		$TESTNET_PARAM \
-		account \
-		validator \
-		create \
-		--wallet-name $WALLET_NAME \
-		--wallet-passphrase $WALLET_PASSFILE \
-		--at-most $VALIDATOR_COUNT &&
-
 	exec lighthouse \
 		--debug-level $DEBUG_LEVEL \
 		$TESTNET_PARAM \
