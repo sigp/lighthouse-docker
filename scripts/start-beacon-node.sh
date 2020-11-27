c1231f9eb4c628d4809a5f972207670d41537e03
etimoz <etimoz@users.noreply.github.com>
2020-11-27 17:40:20 +0100

    changed beacon node flag --eth1-endpoint to --eth1-endpoints to support fallback flags, changed env variable in default.env

--- scripts/start-beacon-node.sh
+++ scripts/start-beacon-node.sh
@@ -25,7 +25,7 @@ exec lighthouse \
 	--debug-level $DEBUG_LEVEL \
 	--testnet $TESTNET \
 	beacon_node \
-	--eth1-endpoint $VOTING_ETH1_NODE \
+	--eth1-endpoints $VOTING_ETH1_NODES \
 	--http \
 	--http-address 0.0.0.0 \
 	$METRICS_PARAMS \
